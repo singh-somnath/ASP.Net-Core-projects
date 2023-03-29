a8ffe66631e0bc9e6319a90fe3188214f3e44d93
somnath singh <somnath@pakur2.onmicrosoft.com>
2023-03-29 21:33:46 +0530

    DBContext Created

---
+++ HotelListing.Dev.API/Data/HotelListingDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Dev.API.Data
+{
+    public class HotelListingDBContext : DbContext
+    {
+        public HotelListingDBContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        
+    }
+}
--- HotelListing.Dev.API/Program.cs
+++ HotelListing.Dev.API/Program.cs
@@ -1,7 +1,10 @@
+using HotelListing.Dev.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("ConnectionSytring");
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -9,6 +12,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Added Entityframework to Project
+builder.Services.AddDbContext<HotelListingDBContext>(
+    dbContextOptionsBuilder =>
+    {
+        dbContextOptionsBuilder.UseSqlServer(connectionString);
+    });
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", options =>
@@ -19,7 +28,9 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Host.UseSerilog((ctx,lc)=> lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration)); 
+builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration));
+
+
 
 var app = builder.Build();
 
